9c65e124540b35629241b9a3223f31ec8b64b06d
Windows <Windows@DESKTOP-CST0DDH.lan>
2025-06-12 00:09:34 -0400

    Created the projecct

---
+++ src/summative/Person.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package summative;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+/**
+ *
+ * @author Windows
+ */
+public class Person {
+    private int x, y;
+    private String name;
+    private int age;
+    private PApplet app;
+    private PImage image;
+    
+    public Person(PApplet p, int x, int y, String name, int age, String imagePath){
+        this.app = p;
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.age = age;
+        this.image = app.loadImage(imagePath);
+    }
+    
+    public void move(int dx, int dy){
+        x += dx;
+        y += dy;
+    }
+    
+    public void draw(){
+        app.image(image, x, y);
+    }
+}
---
+++ src/summative/Sketch.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package summative;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    private Person person;
+    
+    public void settings() {
+        size (1000, 800);
+        person = new Person(this, 200, 200, "Sun Wukong", 0, "images/wukongsprite.png");
+    }
+    
+    public void setup() {
+        background(255,255,255);
+        person.draw();
+    }
+    
+}
+
