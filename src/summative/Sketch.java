e7d3a9a4f8e5d94587eced7d1cb7a73c604fb58a
Windows <Windows@DESKTOP-CST0DDH.lan>
2025-06-15 22:58:27 -0400

    Edited sketch class, added background images

--- src/summative/Sketch.java
+++ src/summative/Sketch.java
@@ -10,24 +10,40 @@ import processing.core.PImage;
 public class Sketch extends PApplet {
     private Person wukong;
     private Person scripture;
+    private Person scripture2;
+    private Person scripture3;
     private PImage introBackground;
+    private PImage collectionBackground1;
+    private PImage collectionBackground2;
+    private PImage collectionBackground3;
     private String[] introDialogue;
     private int dialogueIndex = 0;
     private boolean showIntro = true;
     private boolean moveEnabled = false;
-    private boolean inCollectScene = false;
+    private boolean inCollectionScene = false;
     private boolean scriptureCollected = false;
-
+    private static int scripturesCollectedCount = 0;
+    
     public void settings() {
         size(1000, 800);
     }
 
     public void setup() {
+        // Create Wukong character
         wukong = new Person(this, 400, 300, "Sun Wukong", 0, "images/wukongsprite.png");
-        Person scripture = new Person (this, 0, 0, "Scripture", 0, "images/scripture.png");
+        /// Create first scripture at initial position
+        scripture = new Person(this, 100, 100, "Scripture1", 0, "images/scripture.png");
+        // Create second scripture
+        scripture2 = new Person (this, 700, 200, "Scripture2", 0, "images/scripture.png");
+        //Create third scripture
+        scripture3 = new Person (this, 400, 600, "Scripture3", 0, "images/scripture.png");
         // Load background AFTER settings()
         introBackground = loadImage("images/introBackground.png");
-
+        // Load first collection background image
+        collectionBackground1 = loadImage("images/background1.png");
+        collectionBackground2 = loadImage("images/background2.png");
+        collectionBackground3 = loadImage("images/background3.png");
+        // Load dialogue from file
         DialogueManager dialogueManager = new DialogueManager();
         introDialogue = dialogueManager.loadDialogue("intro.txt");
     }
@@ -50,9 +66,48 @@ public class Sketch extends PApplet {
             } else {
                 showIntro = false;
                 moveEnabled = true;
+                inCollectionScene = true;
             }
-        } else if (inCollectScene) {
+        } else if (inCollectionScene) {
+            // Set appropriate background
+            if (scripturesCollectedCount == 0) {
+                image(collectionBackground1, 0, 0, width, height);
+            } else if (scripturesCollectedCount == 1) {
+                image(collectionBackground2, 0, 0, width, height);
+            } else if (scripturesCollectedCount == 2) {
+                image(collectionBackground3, 0, 0, width, height);
+            }
             
+            // Display collection status text
+            fill(0);
+            textSize(20);
+            text("Collect the scriptures! Scripture " + (scripturesCollectedCount + 1) + " of 3.", 20, 20);
+
+            // Draw Wukong and scripture
+            wukong.draw();
+            // Draw only the active scripture and check for collision
+            if (scripturesCollectedCount == 0 && !scriptureCollected) {
+                scripture.draw();
+                if (wukong.isCollidingWith(scripture)) {
+                    scriptureCollected = true;
+                }
+            } else if (scripturesCollectedCount == 1 && !scriptureCollected) {
+                scripture2.draw();
+                if (wukong.isCollidingWith(scripture2)) {
+                    scriptureCollected = true;
+                }
+            } else if (scripturesCollectedCount == 2 && !scriptureCollected) {
+                scripture3.draw();
+                if (wukong.isCollidingWith(scripture3)) {
+                    scriptureCollected = true;
+                }
+            }
+
+            // If collected, show message
+            if (scriptureCollected) {
+                fill(0, 150, 0);
+                text("Good work! Press ENTER to continue.", width / 3 - 100, height / 2);
+            }
         }
     }
 
@@ -60,9 +115,23 @@ public class Sketch extends PApplet {
         if (showIntro) {
             if (key == ENTER) {
                 showIntro = false;
-                //inColllectionScene = true;
+                inCollectionScene = true;
                 moveEnabled = true;
             }
+        } else if (inCollectionScene && scriptureCollected && key == ENTER){
+            scripturesCollectedCount++;
+            scriptureCollected = false;
+
+                if (scripturesCollectedCount >= 3) {
+                    inCollectionScene = false;
+                    moveEnabled = false;
+                    background(255);
+                    fill(0);
+                    textSize(30);
+                    textAlign(CENTER, CENTER);
+                    text("You collected all scriptures!", width / 2, height / 2);
+                }
+            
         } else if (moveEnabled) {
             if (keyCode == LEFT) {
                 wukong.move(-25, 0);
@@ -72,12 +141,7 @@ public class Sketch extends PApplet {
                 wukong.move(0, -25);
             } else if (keyCode == DOWN) {
                     wukong.move(0, 25);
-            } else if (key == ENTER) {
-                background(0);
-                fill(255);
-                text("Wukong defied Heaven and was cast into jail...", width / 2, height / 2);
-                moveEnabled = true;
-            }
+            } 
         }
     }
 }
